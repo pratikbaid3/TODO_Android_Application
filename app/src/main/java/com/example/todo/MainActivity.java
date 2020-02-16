2f547841244e085f22314739128775b8be584c3d
pratikbaid3 <pratikbaid3@gmail.com>
2020-02-16 16:00:06 +0530

    Designed the card view for the task and Also started implementing the recycler view for the tasks

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,30 +1,99 @@
 package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.github.clans.fab.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener
 {
+    EditText txtDate;
+    RecyclerView taskRecyclerView;
+    FloatingActionButton btnAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton btnAddTask=findViewById(R.id.btnAddTask);
+
+        taskRecyclerView=findViewById(R.id.recycler_view_task);
+
+        btnAddTask=findViewById(R.id.btnAddTask);
         btnAddTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Dialog dialog=new Dialog(MainActivity.this);
-                dialog.setContentView(R.layout.cuaton_dialogue_task);
+                dialog.setContentView(R.layout.custom_dialogue_task);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
+                txtDate=dialog.findViewById(R.id.input_task_date);
+                Button btnSave=dialog.findViewById(R.id.btn_save);
+                Button btnCancil=dialog.findViewById(R.id.btn_cancel);
+
+                txtDate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        showDatePickerDialog();
+                    }
+                });
+
+                btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        //TODO Go from this to initRecyclerView with the task name, task description, task date and the task type
+                        dialog.dismiss();
+                    }
+                });
+
+                btnCancil.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        dialog.dismiss();
+                    }
+                });
             }
         });
 
+    }
+
+    public void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
+    {
+        txtDate.setText(dayOfMonth+"/"+month+"/"+year);
+    }
+
+    private void initRecyclerView()
+    {
+        /**RecyclerView recyclerView = findViewById(R.id.recycler_view_messages);
+        RecyclerViewAdapter_Messages adapter = new RecyclerViewAdapter_Messages(Active_Messages_Page.this, mUsername, mNotification,mUid);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(Active_Messages_Page.this));**/
     }
 }
